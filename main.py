6c29bcd8bffbf442afb70fca949f513b1c882017
dmipor333 <dmipor333@ya.ru>
2024-11-15 18:50:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root() -> dict:
+    return {'message': 'Главная страница'}
+
+
+@app.get("/user/admin")
+async def admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+
+# @app.get("/user/{user_id}")
+# async def users_id(user_id: int) -> dict:
+#     return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+
+# @app.get("/user")
+# async def user(username: str, age: int) -> dict:
+#     return {'message': f'Информация о пользователе. Имя: {username}. Возраст: {age}'}
+
+
+@app.get("/user/{user_id}")
+async def user_page(user_id: int=Path(ge=1, le=100, discription='Enter User ID', example='12')) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+
+@app.get("/user/{username}/{age}")
+async def user(username: Annotated[str, Path(min_length=5, max_length=20, discription='Enter Username',
+            example='UrbanUser')], age: Annotated[int, Path(ge=18, le=120, dis='Enter Age',
+            example='24')]) -> dict:
+    return {'message': f'Информация о пользователе. Имя: {username}. Возраст: {age}'}
